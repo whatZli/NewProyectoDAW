09c142b2e92c078ee256c06d4ace7d51fa6a199c
daw2 <daw2@IS32WX18>
2020-02-07 13:55:34 +0100

    Commit 07/02/2020 3 Proyecto Bien

--- proyectoDWES/proyectoTema6/Proyecto19-20/controller/cAdministrator.php
+++ proyectoDWES/proyectoTema6/Proyecto19-20/controller/cAdministrator.php
@@ -34,8 +34,11 @@ if (isset($_POST['logIn'])) {
             exit;
         } else { // Si $objetoUsuario no tiene un Usuario
             $_SESSION['vista'] = $vistas['administrator']; //Se carga en la sesión de vistas, la que queremos
-            require_once $vistas["layout"];
+            require_once $vistas['layout']; //se incluye la vista que contiene la $_SESSION['vista']
         }
+    } else { // Si $objetoUsuario no tiene un Usuario
+        $_SESSION['vista'] = $vistas['administrator']; //Se carga en la sesión de vistas, la que queremos
+        require_once $vistas['layout']; //se incluye la vista que contiene la $_SESSION['vista']
     }
 } else {
     $_SESSION['vista'] = $vistas['administrator']; //Se carga en la sesión de vistas, la que queremos
